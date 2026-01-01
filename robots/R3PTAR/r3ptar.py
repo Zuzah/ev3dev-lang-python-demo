1b8dff2fa858a748d10ea4bcc8bcbb7f5a3ed02e
Zuzah <murtaza.hasni@gmail.com>
2026-01-01 12:42:35 -0500

    feat(r3ptar): added option for a feed mode to open mouth slowly

--- robots/R3PTAR/r3ptar.py
+++ robots/R3PTAR/r3ptar.py
@@ -11,6 +11,7 @@ import subprocess
 import os
 import time
 from ev3dev2.motor import OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D, MediumMotor, LargeMotor
+from ev3dev2.button import Button
 from ev3dev2.sensor.lego import InfraredSensor
 from ev3dev2.display import Display
 from ev3dev2.sound import Sound
@@ -35,11 +36,17 @@ class MonitorRemoteControl(Thread):
         return "MonitorRemoteControl"
 
     def run(self):
-        STRIKE_SPEED_PCT = 40
         STRIKE_DISTANCE = 30
-        STRIKE_FORWARD_SEC = 0.15
-        STRIKE_BACK_SEC = 0.15
-        STRIKE_COOLDOWN = 0.7
+        FAST_STRIKE_SPEED_PCT = 40
+        FAST_STRIKE_FORWARD_SEC = 0.15
+        FAST_STRIKE_BACK_SEC = 0.15
+        FAST_STRIKE_COOLDOWN = 0.7
+        FEED_STRIKE_SPEED_PCT = 20
+        FEED_STRIKE_PREOPEN_SEC = 1.0
+        FEED_STRIKE_FORWARD_SEC = 0.35
+        FEED_STRIKE_HOLD_SEC = 1.0
+        FEED_STRIKE_BACK_SEC = 0.35
+        FEED_STRIKE_COOLDOWN = 1.2
         RATTLE_INTERVAL = 8.0
         SOUND_COOLDOWN = 0.8
         HISS_SOUND = '/home/robot/R3PTAR/snake-hiss.wav'
@@ -57,9 +64,25 @@ class MonitorRemoteControl(Thread):
 
             now = time.monotonic()
             proximity = self.parent.remote.proximity
+            self.parent.buttons.process()
+
+            if self.parent.feed_mode:
+                strike_speed_pct = FEED_STRIKE_SPEED_PCT
+                strike_preopen_sec = FEED_STRIKE_PREOPEN_SEC
+                strike_forward_sec = FEED_STRIKE_FORWARD_SEC
+                strike_hold_sec = FEED_STRIKE_HOLD_SEC
+                strike_back_sec = FEED_STRIKE_BACK_SEC
+                strike_cooldown = FEED_STRIKE_COOLDOWN
+            else:
+                strike_speed_pct = FAST_STRIKE_SPEED_PCT
+                strike_preopen_sec = 0.0
+                strike_forward_sec = FAST_STRIKE_FORWARD_SEC
+                strike_hold_sec = 0.0
+                strike_back_sec = FAST_STRIKE_BACK_SEC
+                strike_cooldown = FAST_STRIKE_COOLDOWN
 
             if now - last_log_time >= 1.0:
-                log.info('%s: proximity=%s', self, proximity)
+                log.info('%s: proximity=%s mode=%s', self, proximity, self.parent.mode_label())
                 last_log_time = now
 
             if proximity < STRIKE_DISTANCE and now >= next_strike_time:
@@ -73,10 +96,16 @@ class MonitorRemoteControl(Thread):
                 elif not os.path.exists(HISS_SOUND):
                     log.warning('%s: sound file missing: %s', self, HISS_SOUND)
 
-                self.parent.strike_motor.on_for_seconds(speed=STRIKE_SPEED_PCT, seconds=STRIKE_FORWARD_SEC)
-                self.parent.strike_motor.on_for_seconds(speed=(STRIKE_SPEED_PCT * -1), seconds=STRIKE_BACK_SEC)
-                next_strike_time = time.monotonic() + STRIKE_COOLDOWN
+                if strike_preopen_sec > 0:
+                    log.info('%s: feed pre-open delay %.1fs', self, strike_preopen_sec)
+                    sleep(strike_preopen_sec)
+                self.parent.strike_motor.on_for_seconds(speed=strike_speed_pct, seconds=strike_forward_sec)
+                if strike_hold_sec > 0:
+                    sleep(strike_hold_sec)
+                self.parent.strike_motor.on_for_seconds(speed=(strike_speed_pct * -1), seconds=strike_back_sec)
+                next_strike_time = time.monotonic() + strike_cooldown
                 last_rattle_time = time.monotonic()
+                self.parent.show_mode()
             elif now - last_rattle_time >= RATTLE_INTERVAL:
                 if now - last_sound_time >= SOUND_COOLDOWN and os.path.exists(RATTLE_SOUND):
                     log.info('%s: playing rattle sound', self)
@@ -105,11 +134,13 @@ class R3PTAR(object):
         self.speaker.set_volume(100)
         self.screen = Display()
         STEER_SPEED_PCT = 30
+        self.buttons = Button()
+        self.buttons.on_up = self.toggle_mode
+        self.feed_mode = False
 
         self.remote = InfraredSensor()
         log.info('IR sensor address=%s' % self.remote.address)
-        self.screen.text_grid('R3PTAR ready', clear_screen=True)
-        self.screen.update()
+        self.show_mode()
         self.remote.on_channel1_top_left = self.make_move(self.drive_motor, drive_speed_pct)
         self.remote.on_channel1_bottom_left = self.make_move(self.drive_motor, drive_speed_pct * -1)
         self.remote.on_channel1_top_right = self.make_move(self.steer_motor, STEER_SPEED_PCT)
@@ -131,6 +162,20 @@ class R3PTAR(object):
             else:
                 motor.stop()
         return move
+
+    def mode_label(self):
+        return 'FEED' if self.feed_mode else 'FAST'
+
+    def show_mode(self):
+        self.screen.text_grid('Mode: %s\nUP: change mode' % self.mode_label(), clear_screen=True)
+        self.screen.update()
+
+    def toggle_mode(self, state=None):
+        if state is False:
+            return
+        self.feed_mode = not self.feed_mode
+        log.info('Mode changed: %s', self.mode_label())
+        self.show_mode()
 
     def shutdown_robot(self):
 
